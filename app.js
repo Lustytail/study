8bde66c4e9d3304525e94de93a6e62659dfe5c01
Lustytail <qkrehdgns91@gmail.com>
2023-03-28 13:46:30 +0900

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("input");
+
+function loginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", loginSubmit);
